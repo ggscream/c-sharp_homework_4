86d00f63391a7ed5361d35bd19990222248e0276
Katia <puzynia.katia@gmail.com>
2022-12-01 19:37:32 +0300

    Решена вторая задача

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using static System.Console;
+
+WriteLine("Введите целое число: ");
+int c = Convert.ToInt32(ReadLine());
+
+int sum = 0;
+
+while (c != 0)
+{
+    sum += (c % 10);
+    c /= 10;
+}
+
+WriteLine(sum);
