02f7e88f0ae6a317e323f6420dd197add8b03af3
Katia <puzynia.katia@gmail.com>
2022-12-01 19:21:55 +0300

    решена первая задача

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+double Exponentiation(double a, int b) => Math.Pow(a, b);
+
+Console.WriteLine("Введите число А: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B (Строго положительное): ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double c = Exponentiation(a, b);
+Console.WriteLine(c);
